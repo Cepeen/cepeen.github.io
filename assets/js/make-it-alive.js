a5b70e558dfad63797f77890ed3f472458bc5d8b
Tomek Tomasik <32199115+Cepeen@users.noreply.github.com>
2023-10-25 22:22:58 +0200

    navbar

--- assets/js/make-it-alive.js
+++ assets/js/make-it-alive.js
@@ -64,3 +64,13 @@ gsap.utils.toArray("#author").forEach(function (elem) {
 });
 
 
+var prevScrollpos = window.scrollY;
+window.onscroll = function() {
+  var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+} 
