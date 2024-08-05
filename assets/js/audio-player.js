0b5a0aeed7f311417f55176b408e88e3b65b1451
Tomek Tomasik <32199115+Cepeen@users.noreply.github.com>
2024-08-05 19:36:39 +0200

    node tree update

--- assets/js/audio-player.js
+++ assets/js/audio-player.js
@@ -390,205 +390,6 @@ toggleButton.addEventListener('click', toggleScroll);
 
 
 
-
-
-//node tree
-
-
-
-var margin = {
-    top: 20,
-    right: 120,
-    bottom: 20,
-    left: 120
-  },
-  width = 960 - margin.right - margin.left,
-  height = 800 - margin.top - margin.bottom;
-
-var i = 0,
-  duration = 500,
-  root;
-
-var tree = d3.layout.tree()
-  .size([height, width]);
-
-var diagonal = d3.svg.diagonal()
-  .projection(function(d) {
-    return [d.y, d.x];
-  });
-
-var svg = d3.select("#tree-container").append("svg")
-  .attr("width", width + margin.right + margin.left)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-d3.json("/assets/music.json", function(error, genre) {
-  if (error) throw error;
-
-  root = genre;
-  root.x0 = height / 3;
-  root.y0 = 500;
-
-  function collapseAll(d) {
-    if (d.children) {
-      d._children = d.children;
-      d._children.forEach(collapseAll);
-      d.children = null;
-    }
-  }
-
-  collapseAll(root);
-
-  update(root);
-});
-
-d3.select(self.frameElement).style("height", "800px");
-
-function update(source) {
-  var nodes = tree.nodes(root).reverse(),
-    links = tree.links(nodes);
-
-  nodes.forEach(function(d) {
-    d.y = d.depth * 180;
-  });
-
-  var node = svg.selectAll("g.node")
-    .data(nodes, function(d) {
-      return d.id || (d.id = ++i);
-    });
-
-  var nodeEnter = node.enter().append("g")
-    .attr("class", "node")
-    .attr("transform", function(d) {
-      return "translate(" + source.y0 + "," + source.x0 + ")";
-    })
-    .on("click", click);
-
-  nodeEnter.append("rect")
-    .attr("class", "node-bg")
-    .attr("y", -15)
-    .attr("x", function(d) {
-      return d === root ? -60 : -5;
-    })
-    .attr("width", 10)
-    .attr("height", 30)
-    .attr("rx", 10)
-    .attr("ry", 10)
-    .style("fill", function(d) {
-      return d.children || d._children ? "#f0f0f0" : "#000000";
-    })
-    .style("stroke", function(d) {
-      return d.children || d._children ? "#f0f0f0" : "#000000";
-    })
-    .style("stroke-width", 1);
-
-  nodeEnter.append("text")
-    .attr("dy", ".35em")
-    .style("fill", function(d) {
-      return d.children || d._children ? "#000000" : "#ffffff";
-    })
-    .attr("x", function(d) {
-      return d === root ? -15 : 0;
-    })
-    .attr("text-anchor", function(d) {
-      return d === root ? "end" : "start";
-    })
-    .text(function(d) {
-      return d.name;
-    })
-    .style("fill-opacity", 1e-6);
-
-  var nodeUpdate = node.transition()
-    .duration(duration)
-    .attr("transform", function(d) {
-      return "translate(" + d.y + "," + d.x + ")";
-    });
-
-  nodeUpdate.select("rect")
-    .attr("width", function(d) {
-      var textLength = this.parentNode.querySelector("text").getComputedTextLength();
-      return d === root ? Math.max(60, textLength + 10) : Math.max(20, textLength + 10);
-    })
-    .style("fill", function(d) {
-      return d.children || d._children ? "#f0f0f0" : "#000000";
-    })
-    .style("stroke", function(d) {
-      return d.children || d._children ? "#f0f0f0" : "#000000";
-    });
-
-  nodeUpdate.select("text")
-    .style("fill-opacity", 1);
-
-  var nodeExit = node.exit().transition()
-    .duration(duration)
-    .attr("transform", function(d) {
-      return "translate(" + source.y + "," + source.x + ")";
-    })
-    .remove();
-
-  nodeExit.select("rect")
-    .attr("width", 1e-6);
-
-  nodeExit.select("text")
-    .style("fill-opacity", 1e-6);
-
-  var link = svg.selectAll("path.link")
-    .data(links, function(d) {
-      return d.target.id;
-    });
-
-  link.enter().insert("path", "g")
-    .attr("class", "link")
-    .attr("d", function(d) {
-      var o = {
-        x: source.x0,
-        y: source.y0
-      };
-      return diagonal({
-        source: o,
-        target: o
-      });
-    });
-
-  link.transition()
-    .duration(duration)
-    .attr("d", diagonal);
-
-  link.exit().transition()
-    .duration(duration)
-    .attr("d", function(d) {
-      var o = {
-        x: source.x,
-        y: source.y
-      };
-      return diagonal({
-        source: o,
-        target: o
-      });
-    })
-    .remove();
-
-  nodes.forEach(function(d) {
-    d.x0 = d.x;
-    d.y0 = d.y;
-  });
-}
-
-function click(d) {
-  if (d.children) {
-    d._children = d.children;
-    d.children = null;
-  } else if (d._children) {
-    d.children = d._children;
-    d._children = null;
-  } else {
-    playTrackFromServer(d.name);
-  }
-  update(d);
-}
-
-
 async function playTrackFromServer(trackName) {
   try {
     const trackUrl = `https://lastfmfiddler.tomektomasik.pl/all/${encodeURIComponent(trackName)}.mp3`;
@@ -602,7 +403,240 @@ async function playTrackFromServer(trackName) {
   } catch (error) {
     console.error('Error fetching and playing track:', error);
   }
+  }
+
+
+
+
+
+//node tree
+
+var margin = {
+  top: 20,
+  right: 120,
+  bottom: 20,
+  left: 120
+},
+width = 960 - margin.right - margin.left,
+baseHeight = 200 - margin.top - margin.bottom;
+
+var i = 0,
+duration = 500,
+root;
+
+var tree = d3.layout.tree()
+.size([baseHeight, width]);
+
+var diagonal = d3.svg.diagonal()
+.projection(function(d) {
+  return [d.y, d.x];
+});
+
+var svg = d3.select("#tree-container").append("svg")
+.attr("width", width + margin.right + margin.left)
+.attr("height", baseHeight + margin.top + margin.bottom)
+.append("g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json("/assets/music.json", function(error, genre) {
+  if (error) throw error;
+
+  root = genre;
+  root.x0 = baseHeight / 2;
+  root.y0 = 0;
+
+  function collapseAll(d) {
+    if (d.children) {
+      d._children = d.children;
+      d._children.forEach(collapseAll);
+      d.children = null;
+    }
+  }
+
+  collapseAll(root);
+  centerNode(root); 
+  update(root);
+});
+
+d3.select(self.frameElement).style("height", "1000px");
+
+function update(source) {
+centerNode(source);
+
+var nodes = tree.nodes(root).reverse(),
+    links = tree.links(nodes);
+
+var node = svg.selectAll("g.node")
+  .data(nodes, function(d) {
+    return d.id || (d.id = ++i);
+  });
+
+var nodeEnter = node.enter().append("g")
+  .attr("class", "node")
+  .attr("transform", function(d) {
+    return "translate(" + source.y0 + "," + source.x0 + ")";
+  })
+  .on("click", click);
+
+nodeEnter.append("rect")
+  .attr("class", "node-bg")
+  .attr("y", -15)
+  .attr("x", function(d) {
+    return d === root ? -60 : -5;
+  })
+  .attr("width", 10)
+  .attr("height", 30)
+  .attr("rx", 10)
+  .attr("ry", 10)
+  .style("fill", function(d) {
+    return d.children || d._children ? "#f0f0f0" : "#000000";
+  })
+  .style("stroke", function(d) {
+    return d.children || d._children ? "#f0f0f0" : "#000000";
+  })
+  .style("stroke-width", 1);
+
+  nodeEnter.append("text")
+  .attr("dy", ".35em")
+  .style("fill", function(d) {
+    return d.children || d._children ? "#000000" : "#ffffff";
+  })
+  .attr("x", function(d) {
+    return d === root ? -15 : 0;
+  })
+  .attr("text-anchor", function(d) {
+    return d === root ? "end" : "start";
+  })
+  .text(function(d) {
+
+    return d.name + ((!d.children && !d._children) ? " ►" : "");
+  })
+  .style("fill-opacity", 1e-6);
+
+var nodeUpdate = node.transition()
+  .duration(duration)
+  .attr("transform", function(d) {
+    return "translate(" + d.y + "," + d.x + ")";
+  });
+
+nodeUpdate.select("rect")
+  .attr("width", function(d) {
+    var textLength = this.parentNode.querySelector("text").getComputedTextLength();
+    return d === root ? Math.max(60, textLength + 10) : Math.max(20, textLength + 10);
+  })
+  .style("fill", function(d) {
+    return d.children || d._children ? "#f0f0f0" : "#000000";
+  })
+  .style("stroke", function(d) {
+    return d.children || d._children ? "#f0f0f0" : "#000000";
+  });
+
+nodeUpdate.select("text")
+  .style("fill-opacity", 1);
+
+var nodeExit = node.exit().transition()
+  .duration(duration)
+  .attr("transform", function(d) {
+    return "translate(" + source.y + "," + source.x + ")";
+  })
+  .remove();
+
+nodeExit.select("rect")
+  .attr("width", 1e-6);
+
+nodeExit.select("text")
+  .style("fill-opacity", 1e-6);
+
+var link = svg.selectAll("path.link")
+  .data(links, function(d) {
+    return d.target.id;
+  });
+
+link.enter().insert("path", "g")
+  .attr("class", "link")
+  .attr("d", function(d) {
+    var o = {
+      x: source.x0,
+      y: source.y0
+    };
+    return diagonal({
+      source: o,
+      target: o
+    });
+  });
+
+link.transition()
+  .duration(duration)
+  .attr("d", diagonal);
+
+link.exit().transition()
+  .duration(duration)
+  .attr("d", function(d) {
+    var o = {
+      x: source.x,
+      y: source.y
+    };
+    return diagonal({
+      source: o,
+      target: o
+    });
+  })
+  .remove();
+
+nodes.forEach(function(d) {
+  d.x0 = d.x;
+  d.y0 = d.y;
+});
+
+// count number of expanded nodes to calculate new height
+var expandedNodesCount = nodes.filter(function(d) {
+  return d.children && d.children.length > 0;
+}).length;
+
+var newHeight = baseHeight + expandedNodesCount * 70 - 50;
+tree.size([newHeight, width]);
+
+d3.select("svg").transition().duration(duration)
+  .attr("height", newHeight + margin.top + margin.bottom);
+
+svg.transition().duration(duration)
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+}
+
+function click(d) {
+if (d.children) {
+  d._children = d.children;
+  d.children = null;
+} else if (d._children) {
+  d.children = d._children;
+  d._children = null;
+} else {
+  playTrackFromServer(d.name);
+  return; 
+}
+
+update(d);
 }
 
 
+function centerNode(source) {
+var nodes = tree.nodes(root);
+var height = Math.max(100, nodes.length * 30);
+var depth = Math.max.apply(Math, nodes.map(function(d) { return d.depth; }));
+var newHeight = Math.max(baseHeight, height);
 
+tree.size([newHeight, width]);
+
+var scale = newHeight / height;
+nodes.forEach(function(d) {
+  d.y = d.depth * 180;
+  d.x = (d.x * scale) + (newHeight - height * scale) / 2;
+});
+
+// SVG height update
+d3.select("svg").transition().duration(duration)
+  .attr("height", newHeight + margin.top + margin.bottom);
+
+svg.transition().duration(duration)
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+}
